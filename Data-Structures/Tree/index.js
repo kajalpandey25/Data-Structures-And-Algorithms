71ab66012b69ebce7c454e7fb8908b091d5cbe2a
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-05-14 12:34:27 +0530

    ok

--- Data-Structures/Tree/index.js
+++ Data-Structures/Tree/index.js
@@ -1,18 +1,45 @@
-class Node{
-    constructor(val)
-    {
-       this.value=val;
-       this.left=null;
-       this.right=null; 
-    }
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 class BSTree {
   constructor() {
     this.root = null;
   }
-  isTreeEmpty(){
-    return this.root===null;
+  isTreeEmpty() {
+    return this.root === null;
+  }
+  makeTree(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // Inserting Node
+  insertNode(root, newNode) {
+    if (root.value > newNode.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    }else{
+      if(root.left === null){
+        root.left = newNode;
+      }else{
+        this.insertNode(root.right, newNode)
+      }
+    }
   }
 }
 let bst = new BSTree();
-console.log(bst.isTreeEmpty());
+bst.makeTree(20);
+bst.makeTree(10);
+bst.makeTree(5);
+console.log(bst);
