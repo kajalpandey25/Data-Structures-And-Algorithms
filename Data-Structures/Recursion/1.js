2ca4e249241a8541deb25bda82522122f805d83c
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-05-18 22:04:13 +0530

    commit

--- Data-Structures/Recursion/1.js
+++ Data-Structures/Recursion/1.js
@@ -17,4 +17,43 @@ function fibRecursion(n) {
   return fibRecursion(n - 1) + fibRecursion(n - 2);
 }
 
-console.log(fibRecursion(15));
+// console.log(fibRecursion(15));
+
+// Q-2 : Reverse a String
+//  Input: "kajal"   ----------->>>>>>> output: "lajak"
+
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  } else return reverseString(str.substr(1)) + str.charAt(0);
+}
+
+// console.log(reverseString("kajal"));
+
+// Q-3 : Subset (Backtracking Algorithms using Recursion)
+// Given an integer array nums of unique elements, return all possible subsets (the power set)
+// The solution set must not contain duplicate subset. Return the solution in any order.
+
+// Input: [1,2,3]   -------->>>>>>>>> output: [[],[1], [2], [1,2],[3], [1,3], [2,3], [1,2,3]]
+// Input: [0]   ------------>>>>>>>> output: [[], [0]]
+
+function subsets(nums) {
+  let result = [];
+  let temp = [];
+
+  function recursiveSubsets(nums, i) {
+    if(i === nums.length){
+      return result.push([...temp]);
+    }
+    temp.push(nums[i])
+    recursiveSubsets(nums, i+1);
+    temp.pop();
+    recursiveSubsets(nums, i+1);
+  
+  }
+  recursiveSubsets(nums, 0);
+  return result;
+}
+
+console.log(subsets([1]));
+console.log(subsets([1,2,3]));
