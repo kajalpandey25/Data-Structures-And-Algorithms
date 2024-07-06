1173e8a97de50103537d86154384ce7946e5a5e3
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-07-06 17:10:10 +0530

    commit

---
+++ JS/higher-order-function.js
@@ -0,0 +1,22 @@
+function salary(num){
+    return num * .7
+}
+
+setTimeout(salary(), 300)
+
+const mySalaries = [50, 40, 60, 20, 300, 400]
+
+mySalaries.filter(salary).filter(tax).map()
+
+// () => {}
+// (func) => {}
+// (func) => () => {}
+// (func) => async() => {}
+
+const asyncHandler =  (fn) => async(req, res, next)=>{
+    try{
+        await fn(req, res, next)
+    } catch(error){
+        
+    }
+}
